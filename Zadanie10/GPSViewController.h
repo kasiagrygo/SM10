12c2f218c462f16ca450f07b65d61791554627bc
kasiagrygo <katarzynagrygo@icloud.com>
2023-12-18 04:01:03 +0100

    Initial commit

---
+++ Zadanie10/GPSViewController.h
@@ -0,0 +1,17 @@
+#import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GPSViewController : UIViewController <CLLocationManagerDelegate>
+
+@property (weak, nonatomic) IBOutlet UILabel *latitudeValueLabel;
+@property (weak, nonatomic) IBOutlet UILabel *longtitudeValueLabel;
+@property (weak, nonatomic) IBOutlet UILabel *addressValueLabel;
+@property (weak, nonatomic) IBOutlet UIButton *currentLocationButton;
+
+- (IBAction)getCurrentLocation:(id)sender;
+
+@end
+
+NS_ASSUME_NONNULL_END
